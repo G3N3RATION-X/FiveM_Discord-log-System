3587fd46f66b2eb77ddb213a3f43617d18de8c5e
G3N3RATION-X <102898784+G3N3RATION-X@users.noreply.github.com>
2023-11-15 16:44:02 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description "®©BYLICKILABS </ FiveM/Discord - Logger System"
+
+author 		'Thorsten Bylicki'
+name 		'BYLICKILABS'
+version 	'v1'
+
+
+server_scripts{
+    'G3N-X_server_main.lua',
+    'config.lua',
+}
